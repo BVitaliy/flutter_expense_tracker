44b56c98fee095e315440e94e5064b104d2ed1e4
BVitaliy <v.belza008@gmail.com>
2026-02-15 21:03:36 +0200

    update

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:flutter_expense_tracker/models/expense.dart';
 
 class Expenses extends StatefulWidget {
@@ -21,11 +22,28 @@ class _ExpensesState extends State<Expenses> {
       amount: 12.45,
       date: DateTime.now(),
       category: Category.leisure,
-    ),    
+    ),
   ];
+
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(context: context, builder: (ctx) => Text('Modal asd as das d'));
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Column(children: [Text('The chart')]));
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Expense Tracker'),
+        actions: [
+          IconButton(onPressed: _openAddExpenseOverlay, icon: Icon(Icons.add)),
+        ],
+      ),
+      body: Column(
+        children: [
+          const Text('The chart'),
+          Expanded(child: ExpensesList(expenses: _registeredExpenses)),
+        ],
+      ),
+    );
   }
 }
